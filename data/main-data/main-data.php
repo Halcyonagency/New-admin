8e5d2efb405ae80ac27a4b9f32e3f5aee36d8fed
Horacio <horacio@pampanetwork.com>
2014-12-05 20:50:22 -0300

    Agrego nuevos archivos e instalo plugin de carga de info

--- data/main-data/main-data.php
+++ data/main-data/main-data.php
@@ -1,10 +1,20 @@
 <?php 
 
-$selection=$_GET["selection"];
-
-echo $selections;
+$selection=$_POST["selection"];
 
 
+
+if (isset($selection)) {
+
+$textos = array('Nosotros' => "Breve reseña historica...","Portfolio"=>"Mirá los últimos trabajos:", "Blog"=>"Dejanos tu opinión y leé la nuestra", "Contacto"=>"Contactanos!" );
+	
+
+	
+	echo json_encode($textos[$selection]);
+	
 }
+
+
+
 
  ?>
